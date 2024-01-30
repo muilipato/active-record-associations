389aacc97f608d47889ce814c33594b4eec78d78
Patrick <muilipato@gmail.com>
2024-01-30 13:02:40 +0300

    Establish the many-to-many (or has many through) association in Active Record '

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ActiveRecord::Base
   belongs_to :game #Active record macro
+  belongs_to :user
 end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  has_many :reviews
+  has_many :games, through: :reviews
+
+end
